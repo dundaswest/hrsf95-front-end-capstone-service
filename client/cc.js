8f13a808bc349c90b120fd4412e2177225eb39fe
Eunbyul Cho <hello@eunbyulcho.me>
2018-05-30 19:05:32 -0700

    initial commit

---
+++ client/cc.js
@@ -0,0 +1,12 @@
++---------+-------------+------+-----+---------+-------+
+| Field   | Type        | Null | Key | Default | Extra |
++---------+-------------+------+-----+---------+-------+
+| name    | varchar(20) |NOTNULL|     | NULL    |       |
+|userPhoto| varchar(70) |NOTNULL|     | NULL    |       |
+| date    | varchar(20) |NOTNULL|     | NULL    |       |
+| rating | char(1)      |NOTNULL|     | NULL    |       |
+| comment | varchar(50) |NOTNULL|     | NULL    |       |
+|   |
++---------+-------------+------+-----+---------+-------+
+
+name:String, photo:String(url),date:String, rating:Number, comment:String
